c4b13a1fe1f2bad1dd21f72d7e1b4501899467f0
root <root@node00.ose.jbrideratwater.com>
2015-08-19 22:56:07 -0400

    latest fix for maven

--- 1.6/run.sh
+++ 1.6/run.sh
@@ -41,6 +41,7 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -Dfile.encoding=UTF8 -jar /usr/lib/jenkins/jenkins.war $JENKINS_OPTS "$@"
 fi
 
+
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
 exec "$@"
 
